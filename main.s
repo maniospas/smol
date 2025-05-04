27979d77d59a40ab81a4adcbe3a53119c2ff8166
maniospas <maniospas@hotmail.com>
2025-05-04 21:31:50 +0300

    github link in docs

--- main.s
+++ main.s
@@ -1,12 +1,10 @@
 @include std
-union Type(i64, f64)
+union Type(i64, f64, u64)
 smo Point(Type x, Type y) -> @new
-
 smo inc(Point &p)
     p.x = add(p.x, Type(1))
     p.y = add(p.y, Type(1))
     --
-
 smo main()
     value = i64|read()
     p = Point(value, value)
