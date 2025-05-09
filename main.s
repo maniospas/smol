1772d6a523271f49d85fd1f9205844623f83bd08
maniospas <maniospas@hotmail.com>
2025-05-10 02:33:03 +0300

    updated docs

--- main.s
+++ main.s
@@ -1,6 +1,10 @@
 @include std
 
+service square(f64 x) 
+    fail("Don't wanna!") // manually fail
+    -> mul(x,x)
 service main()
-    success = false
-    x = f64:read()
-    print(x)
+    y = square(2.0)
+    if(y.err:bool) print("Something went wrong") --
+    else print(y) --
+    --
