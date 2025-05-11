0776e00fe8434bd41aa06854cbaf8156a063a554
maniospas <maniospas@hotmail.com>
2025-05-11 22:14:53 +0300

    multiple returns from services (still need to defer deallocation)

--- main.s
+++ main.s
@@ -1,11 +1,11 @@
 @include std
 
-smo addmut(str &v1, str &v2)
+smo add2(str v1, str v2)
     v1 = v1:add(v2)
-    --
+    -> v1
 
 service main()
     r1 = "aa":str
     r2 = "bb":str
-    addmut(r1,r2)
+    r1 = add2(r1,r2)
     print(r1)
--- parser/parse_return.cpp
+++ parser/parse_return.cpp
@@ -2,17 +2,17 @@ void parse_return(const shared_ptr<Import>& imp, size_t& p, string next, Memory&
     if(imp->at(p)=="-") {++p;return;}
     if(imp->at(p++)!=">") imp->error(p-2, "Use `->` to return a value or `--` to return without a value for expressions starting with `-`");
     next = imp->at(p++);
+    bool hasComma = false;
+    if(is_service) {
+        packs.push_back("err");
+        internalTypes.vars["err"] = types.vars["errcode"];
+    }
     if(next=="@") {
         next = imp->at(p++);
-        /*if(next == "scope") packs.push_back("@scope");
-        else*/ {
-            if(next!="new") imp->error(--p, "Use `->@new`");
-            if(is_service)  packs.push_back("err");
-            internalTypes.vars["err"] = types.vars["errcode"];
-            for (const auto& arg : args) packs.push_back(arg.name);
-            //unordered_set<string> packSet(packs.begin(), packs.end());
-            //for (const auto& arg : internalTypes.vars) if (packSet.insert(arg.first).second) packs.push_back(arg.first);
-        }
+        if(next!="new") imp->error(--p, "Use `->@new`");
+        if(is_service)  packs.push_back("err");
+        internalTypes.vars["err"] = types.vars["errcode"];
+        for (const auto& arg : args) packs.push_back(arg.name);
     }
     else {
         --p;
@@ -21,8 +21,6 @@ void parse_return(const shared_ptr<Import>& imp, size_t& p, string next, Memory&
             next = parse_expression(imp, p, imp->at(p++), types);
             if(!internalTypes.vars[next]->not_primitive()) {
                 if(is_service) {
-                    packs.push_back("err");
-                    internalTypes.vars["err"] = types.vars["errcode"];
                     implementation += "__value = "+next+";\n";
                     if(internalTypes.contains("__value") && internalTypes.vars["__value"]!=internalTypes.vars[next]) imp->error(--p, "Returning single value of multple types "+internalTypes.vars["__value"]->name+" and "+internalTypes.vars[next]->name);
                     internalTypes.vars["__value"] = internalTypes.vars[next];
@@ -31,25 +29,21 @@ void parse_return(const shared_ptr<Import>& imp, size_t& p, string next, Memory&
                 else packs.push_back(next);
             }
             else {
-                if(is_service) {
-                    packs.push_back("__err");
-                    internalTypes.vars["err"] = types.vars["errcode"];
-                    imp->error(--p, "Service multivalue returns not implemented yet");
-                }
                 if(internalTypes.vars.find(next)==internalTypes.vars.end()) imp->error(--p, "Symbol not declared: "+pretty_var(next)+recommend_variable(types, next));
-                if(p && imp->at(p-1)==")") {
+                if(!hasComma && p<imp->size() && imp->at(p)!=",") {
                     // if we are directly returning a proxy, unpack that proxy here
                     for(const string& pack : internalTypes.vars[next]->packs) {
                         assign_variable(internalTypes.vars[next]->internalTypes.vars[pack], pack, next+"__"+pack, imp, p);
                         packs.push_back(pack);
                     }
                 }
-                else for(const string& pack : internalTypes.vars.find(next)->second->packs) packs.push_back(next+"__"+pack);
+                else for(const string& pack : internalTypes.vars[next]->packs) packs.push_back(next+"__"+pack);
             }
             if(p>=imp->size()){break;}
             next = imp->at(p++);
             //if(next==")") break;
             if(next!=",") {--p;break;}//imp->error(--p, "Missing comma (not implemented expression in return statements yet)");
+            hasComma = true;
         }
     }
 }
