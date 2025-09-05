27e7ee99c4d8f738d5d93f2f548562d1bbc67dd3
maniospas <maniospas@hotmail.com>
2025-09-06 00:38:04 +0300

    improved docs

---
+++ tests/app/index.s
@@ -0,0 +1,26 @@
+@include std.builtins
+@include std.mem
+
+smo Segment(nominal, str value)
+    -> @args // return all inputs
+
+smo Segment(String _value)
+    value = _value:str // convert from many string types
+    -> nominal:Segment(value)
+
+smo combine(Segment[] segments)
+    &combined = "":str  // mutable null-terminated string
+    on Stack:arena(1024)
+        range(segments:len)
+        :while next(u64 &i)
+            combined = str(combined+segments[i].value+" ")
+            --
+        --
+    -> combined
+
+service main()
+    segments = Segment[] // buffer
+    :push("I think.":Segment)
+    :push("Therefore I am.":Segment)
+    print(segments:combine)
+    --
---
+++ tests/unit/union.s
@@ -0,0 +1,26 @@
+@include std.builtins
+
+union Type
+    i64 
+    f64 
+    u64
+    --
+
+smo Point(Type _x, Type _y)
+    &x = _x
+    &y = _y
+    -> x,y
+
+smo inc(Point &p)
+    p.x = p.x+Point.Type(1)
+    p.y = p.y+Point.Type(1)
+    --
+
+service main()
+    //print("Give a value: ")
+    //&value = f64:read
+    value = 1:f64
+    p = Point(value, value)
+    p:inc()
+    print(p.x)
+    --
