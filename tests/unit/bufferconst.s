fe9b1fc8cfd4ee3dcb1d792944a7e9b282307334
maniospas <maniospas@hotmail.com>
2025-09-08 17:44:20 +0300

    ready to force

--- tests/unit/bufferconst.s
+++ tests/unit/bufferconst.s
@@ -1,10 +1,11 @@
 @include std.builtins
 @include std.mem
+@unsafe 
 
 smo point(f64 x, f64 y) 
     -> @args
 
-service create()
+smo create()
     p = point[Heap:allocate(1024)]  // 1kB buffer
     :push(point(1.5, 2.2))
     :push(point(0.5, 4.0))
